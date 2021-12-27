ac532d1092d81bca841d3169c8f01d10d644efaf
Kudo1226398122 <1226398122abc@gmail.com>
2021-12-27 10:28:44 +0800

    修改bug

--- src/main/resources/static/js/conference_index.js
+++ src/main/resources/static/js/conference_index.js
@@ -440,7 +440,8 @@ var Page = function() {
 
 
 
-function getCookie(objName) {//获取指定名称的cookie的值
+function getCookie(objName) {
+    //获取指定名称的cookie的值
     var arrStr = document.cookie.split("; ");
     for (var i = 0; i < arrStr.length; i++) {
         var temp = arrStr[i].split("=");
